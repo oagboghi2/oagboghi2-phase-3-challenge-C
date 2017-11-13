49d66550f5e2a654931f5e2751a4d53105fe6f86
Obo Agboghidi <obo@Obos-MacBook-Pro.local>
2017-11-13 14:32:39 -0800

    finished part 1

---
+++ part-1/app.js
@@ -0,0 +1,21 @@
+//dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false }));
+
+//routes
+const mainRoutes = require('./routes/routes.js');
+app.use(mainRoutes);
+
+app.get('/', function(req, res, next){
+  res.send("Hello to you");
+});
+
+//port
+app.listen(3000, function(){
+  console.log("The backend is in another castle");
+})
